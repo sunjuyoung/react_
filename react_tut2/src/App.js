337a8b2602dbce25976e909a6fcf504e1721db05
syseoz <syseoz@naver.com>
2022-07-14 17:08:15 +0900

    todo class ver

--- react_tut2/src/App.js
+++ react_tut2/src/App.js
@@ -21,7 +21,8 @@ export default class App extends Component {
         title: "공부하기2",
         completed : false
       },
-    ]
+    ],
+    value : "",
   }
 
   btnStyle ={
@@ -33,33 +34,64 @@ export default class App extends Component {
     float: "right"
   }
 
-  getStyle = () =>{
+  getStyle = (completed) =>{
     return {
       padding : "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none"
+      textDecoration: completed ? "line-through"  : "none"
     }
   }
 
-
+  
 
   handleClick=(id)=>{
     let newTodoData = this.state.todoData.filter((data)=> data.id !== id);
       this.setState({todoData:newTodoData});
+  }
+  handleChange=(e)=>{
+    console.log(e.target.value);
+    this.setState({value:e.target.value})
+  }
+  handleSubmit=(e)=>{
+    
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title : this.state.value,
+      completed : false,
+    }
+
+    this.setState({todoData: [...this.state.todoData, newTodo] });
+    this.setState({value:""});
+  };
+
+  handleCheckBox=(id)=>{
+    let newData =this.state.todoData.map((data)=>{
+        if(data.id === id){
+            data.completed = !data.completed;
+        }
+        return data;
+      });
+      this.setState({todoData:newData});
   }
 
   render() {
     return (
       <div className='container'>
         <div className='todoBlock'>
-          <input type="text" placeholder='새로 할 일을 입력하세요'></input> 
-          <button>입력</button>
+          <form style={{display:'flex'}} onSubmit={this.handleSubmit}>
+          <input type="text" placeholder='새로 할 일을 입력하세요' name='value' onChange={this.handleChange} 
+          style={{ flex: '10',padding:'5px'}}  /> 
+          <input type="submit" className='btn' style={{flex : '1'}} value="입력"  />
+          </form>
           <div className='title'>
             <h1>할일 목록</h1>
 
             {this.state.todoData.map((data) => (
-                       <div key={data.id} style={this.getStyle()}>
-                       <input type='checkbox'  defaultChecked={data.completed}/> {data.title}
+                       <div key={data.id} style={this.getStyle(data.completed)} >
+                       <input type='checkbox'  defaultChecked={data.completed} 
+                       onClick={()=> this.handleCheckBox(data.id)} /> {data.title}
                        <button className='xbutton' style={this.btnStyle} 
                        onClick={()=>this.handleClick(data.id)}>x</button>
                      </div>
