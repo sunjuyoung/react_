b5f40f5e34ff11961bb327dc5f904a9ef1ac3aeb
syseoz <syseoz@naver.com>
2022-07-15 10:51:22 +0900

    todo func ver

--- react_tut2/src/App.js
+++ react_tut2/src/App.js
@@ -1,106 +1,94 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-export default class App extends Component {
 
 
-  state = {
-    todoData : [
-      {
-        id: "1",
-        title: "공부하기",
-        completed : true
-      },
-      {
-        id: "2",
-        title: "공부하기1",
-        completed : false
-      },
-      {
-        id: "3",
-        title: "공부하기2",
-        completed : false
-      },
-    ],
-    value : "",
+
+const App = () => {
+
+const [todoData, setTodoData] = useState([]);
+const [value, setValue] = useState("");
+
+
+const btnStyle ={
+  color:"#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor:"pointer",
+  float: "right"
+}
+
+const getStyle = (completed) =>{
+  return {
+    padding : "10px",
+    borderBottom: "1px #ccc dotted",
+    textDecoration: completed ? "line-through"  : "none"
+  }
+}
+
+
+
+const handleClick=(id)=>{
+  let newTodoData = todoData.filter((data)=> data.id !== id);
+    setTodoData(newTodoData);
+}
+const handleChange=(e)=>{
+  setValue(e.target.value);
+}
+const handleSubmit=(e)=>{
+  e.preventDefault();
+
+  let newTodo = {
+    id: Date.now(),
+    title : value,
+    completed : false,
   }
 
-  btnStyle ={
-    color:"#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor:"pointer",
-    float: "right"
-  }
+ // setTodoData({todoData: [...this.state.todoData, newTodo] });
+  //setter에서 이전 state를 가지고 오기 위해서는 인수에 함수를 이용해서 사용할 수 있다
+  setTodoData((prev) => [...prev,newTodo]);
+  setValue("");
+};
 
-  getStyle = (completed) =>{
-    return {
-      padding : "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through"  : "none"
-    }
-  }
+const handleCheckBox=(id)=>{
+  let newData =todoData.map((data)=>{
+      if(data.id === id){
+          data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(newData);
+}
 
-  
 
-  handleClick=(id)=>{
-    let newTodoData = this.state.todoData.filter((data)=> data.id !== id);
-      this.setState({todoData:newTodoData});
-  }
-  handleChange=(e)=>{
-    console.log(e.target.value);
-    this.setState({value:e.target.value})
-  }
-  handleSubmit=(e)=>{
-    
-    e.preventDefault();
-
-    let newTodo = {
-      id: Date.now(),
-      title : this.state.value,
-      completed : false,
-    }
-
-    this.setState({todoData: [...this.state.todoData, newTodo] });
-    this.setState({value:""});
-  };
-
-  handleCheckBox=(id)=>{
-    let newData =this.state.todoData.map((data)=>{
-        if(data.id === id){
-            data.completed = !data.completed;
-        }
-        return data;
-      });
-      this.setState({todoData:newData});
-  }
-
-  render() {
-    return (
+  return (
+    <div>
       <div className='container'>
         <div className='todoBlock'>
-          <form style={{display:'flex'}} onSubmit={this.handleSubmit}>
-          <input type="text" placeholder='새로 할 일을 입력하세요' name='value' onChange={this.handleChange} 
+          <form style={{display:'flex'}} onSubmit={handleSubmit}>
+          <input type="text" placeholder='새로 할 일을 입력하세요' name='value' onChange={handleChange} 
           style={{ flex: '10',padding:'5px'}}  /> 
           <input type="submit" className='btn' style={{flex : '1'}} value="입력"  />
           </form>
           <div className='title'>
             <h1>할일 목록</h1>
 
-            {this.state.todoData.map((data) => (
-                       <div key={data.id} style={this.getStyle(data.completed)} >
+            {todoData.map((data) => (
+                       <div key={data.id} style={getStyle(data.completed)} >
                        <input type='checkbox'  defaultChecked={data.completed} 
-                       onClick={()=> this.handleCheckBox(data.id)} /> {data.title}
-                       <button className='xbutton' style={this.btnStyle} 
-                       onClick={()=>this.handleClick(data.id)}>x</button>
+                       onClick={()=> handleCheckBox(data.id)} /> {data.title}
+                       <button className='xbutton' style={btnStyle} 
+                       onClick={()=>handleClick(data.id)}>x</button>
                      </div>
             ))}
        
-
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
+
