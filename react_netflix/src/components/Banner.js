a8e5d1410f5b8193c02ebc0c3cb19b7da6944088
syseoz <syseoz@naver.com>
2022-07-23 10:47:10 +0900

    banner

--- react_netflix/src/components/Banner.js
+++ react_netflix/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import axios from "../api/axios";
 import requests from "../api/requests";
+import "./Banner.css";
 
 
 const Banner = () => {
@@ -27,8 +28,39 @@ const Banner = () => {
         console.log(movieDetail);
     }
 
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n-1) + "...." : str;
+    }
+
   return (
-    <div>Banner</div>
+    <header
+        className='banner'
+        style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+            backgroundPosition: "top center",
+            backgroundSize: "cover",
+        }}
+    >
+
+        <div className="banner__contents">
+
+            <h1 className='banner__title'>
+                {movie?.title || movie?.name ||movie?.original_name}
+            </h1>
+            <div className='banner__buttons'>
+                <button className='banner__button play'>Play</button>
+                <button className='banner__button info'>
+                    <div className='space'>More Information</div>
+                </button>
+            </div>
+
+            <h1 className='banner__description'>
+                {truncate(movie?.overview, 100)}
+            </h1>
+
+        </div>
+        <div className='banner--fadeBottom'></div>
+    </header>
   )
 }
 
